697ad71ca61b97c5170bbc8849775afdac4cfc98
김민지 <wehouse201@naver.com>
2023-03-12 17:58:47 +0900

    page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import {Routes, Route} from "react-router-com";
+import React from "react";
+import {Routes, Route, Link} from "react-router-dom";
 import Main from "./page/Main.js"
+import Login from "./page/user/Login.js"
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path='/' element={<Login/>}/>
         <Route path='/main' element={<Main/>}/>
       </Routes>
     </div>
---
+++ src/page/user/Login.js
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default Login;
