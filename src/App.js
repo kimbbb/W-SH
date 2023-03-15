e6a7d889156478040ef7ba2243373bd48ef60a79
김민지 <wehouse201@naver.com>
2023-03-15 17:04:55 +0900

    list

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./page/main/Main.js"
 import Login from "./page/user/Login.js"
 import Sign from "./page/user/Sign.js"
 import New from "./page/item/New.js"
+import List from "./page/item/List.js"
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/sign' element={<Sign/>}/>
         <Route path='/main' element={<Main/>}/>
         <Route path='/new' element={<New/>}/>
+        <Route path='/list' element={<List/>}/>
       </Routes>
     </div>
   );
--- src/page/components/Header.js
+++ src/page/components/Header.js
@@ -10,7 +10,7 @@ function Header() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">조회</Nav.Link>
+            <Nav.Link href="/list">조회</Nav.Link>
             <Nav.Link href="/new">생성</Nav.Link>
             <Nav.Link className='out' href="/">로그아웃</Nav.Link>
           </Nav>
