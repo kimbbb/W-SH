ff9c7a7d950c0543e7bc6eb83fa07511aaedeaf3
김민지 <wehouse201@naver.com>
2023-03-19 20:27:41 +0900

    card

--- src/page/item/List.js
+++ src/page/item/List.js
@@ -17,6 +17,30 @@ function List(){
           <C.detailbtn>GO</C.detailbtn>
         </C.bottom>
       </C.card>
+
+      <C.card>
+        <C.top/>
+        <C.bottom>
+          <C.titlebox>
+            <C.title>상품명</C.title>
+            <C.state/>
+          </C.titlebox>
+          <C.cash>₩ 가격</C.cash>
+          <C.detailbtn>GO</C.detailbtn>
+        </C.bottom>
+      </C.card>
+
+      <C.card>
+        <C.top/>
+        <C.bottom>
+          <C.titlebox>
+            <C.title>상품명</C.title>
+            <C.state/>
+          </C.titlebox>
+          <C.cash>₩ 가격</C.cash>
+          <C.detailbtn>GO</C.detailbtn>
+        </C.bottom>
+      </C.card>
     </C.cardbox>
     </>
   )
